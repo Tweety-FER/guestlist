914dcc80f020c9d47ddb7b2e1c2d13cf01912002
Luka Skukan <lskukan1@gmail.com>
2015-11-01 21:19:40 +0100

    Change site title to Guestlist

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Brucosijada Guestlist</title>
+    <title>Guestlist</title>
     <link rel="stylesheet" href="/css/app.css" charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <script src="/js/all.js" charset="utf-8"></script>
