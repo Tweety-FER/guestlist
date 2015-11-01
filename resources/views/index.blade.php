8123a04cb0babb12e13d3c97852f2e9e74e7d3a0
Luka Skukan <lskukan1@gmail.com>
2015-11-01 17:01:07 +0100

    Set viewport for mobile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <title>Brucosijada Guestlist</title>
     <link rel="stylesheet" href="/css/app.css" charset="utf-8">
     <script src="/js/all.js" charset="utf-8"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body ng-app="kset.guestlist">
     <div ui-view></div>
