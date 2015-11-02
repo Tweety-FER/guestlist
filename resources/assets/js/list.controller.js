6da7ab0e501d8674dd0353f5f7d436ffc4d71bc3
Luka Skukan <lskukan1@gmail.com>
2015-11-02 11:59:15 +0100

    Remove test debug

--- resources/assets/js/list.controller.js
+++ resources/assets/js/list.controller.js
@@ -75,7 +75,6 @@
     }
 
     function remove(guest) {
-      console.log('DEBUG', guest);
       $confirm(function() {
         res.delete({id : guest.id}, load);
       }, 'Makni osobu ' + guest.fullName + ' s popisa?');
